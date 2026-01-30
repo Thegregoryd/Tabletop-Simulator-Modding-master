370b534f793777da34c77cb542c77721d031ab63
thegregoryd <gdestes@gmail.com>
2026-01-29 21:53:46 -0500

    Updated to include more recent material files
    NOTE: It's been a while since I've used the Tabletop Simulator and I see they have a new version. I haven't had a chance to test this template against the new one to know what might be missing on that side.

---
+++ Assets/JAMM/Material/Lava.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lava : MonoBehaviour 
+{
+	//public int materialIndex = 0;
+
+	public Vector2 uvAnimationRate = new Vector2( 1.0f, 0.0f );
+	public string textureName = "_MainTex";
+
+
+	Vector2 uvOffset = Vector2.zero; //old
+
+	public float twerk = 1;
+	float scrollSpeed = 0.0025f;
+	Renderer rend;
+
+	void Start()
+	{
+		rend = GetComponent<Renderer>();
+	}
+
+	void Update()
+	{
+		float offset = Time.time * scrollSpeed * twerk;
+		//uvoffset = Time.deltaTime * uvAnimationRate / 40;
+		rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+	}
+
+
+
+	/*
+	void LateUpdate() 
+	{
+		uvOffset += ( uvAnimationRate /40 * Time.deltaTime );
+		if( GetComponent<Renderer>().enabled )
+		{
+			GetComponent<Renderer>().sharedMaterial.SetTextureOffset( textureName, uvOffset );
+		}
+	}
+	*/
+}
